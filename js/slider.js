c6099fbdc765b5529ed6cdeddf174e7396711c53
JinMo-source <qkrwslah123@naver.com>
2021-05-18 05:43:11 +0900

    slider작성

--- js/slider.js
+++ js/slider.js
@@ -1,17 +1,48 @@
 const slider = document.querySelector('.slider-wrap');
-const sliderWidth = slider.offsetWidth;
 const sliderMove = document.querySelector('.slider-trans');
 const nextBotton = document.querySelector('.next');
+const sliderUL = document.querySelector('.slider-back');
 
+const sliderWidth = slider.offsetWidth;
 const sliderMoveStyle = sliderMove.style;
+
+const sliderLength = document.querySelectorAll('.slider-Item');
+const sliderNavi = document.querySelector('.slider-navi');
+
 let current = 0;
+
+function sl(){
+    if(sliderWidth <= Math.abs(current)){
+        current=0;
+        sliderMoveStyle.transform = `translateX(${current}px)`;
+    }else {
+        current = current-sliderWidth;
+        sliderMoveStyle.transform = `translateX(${current}px)`;
+
+    }
+}
+
 
 
 function sliderNextHandle(){
-    current = current-sliderWidth;
-    sliderMoveStyle.transform = `translateX(${current}px)`;
-    console.log(sliderMove.style);
-    console.log(sliderMove.style.transform);
+    if(sliderWidth <= Math.abs(current)){
+        current=0;
+        sliderMoveStyle.transform = `translateX(${current}px)`;
+    }else {
+        current = current-sliderWidth;
+        sliderMoveStyle.transform = `translateX(${current}px)`;
+
+    }
 }
 
+
+setInterval(sl,1000);
+
 nextBotton.addEventListener('click',sliderNextHandle);
+
+function naviLI(){
+    sliderLength.forEach((value,ind) => {
+    `<li class= 'navi-ID__${ind} navi'>${ind}</li>`
+})
+}
+sliderNavi.insertAdjacentHTML('afterbegin', naviLI);
