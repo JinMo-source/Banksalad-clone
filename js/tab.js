a6fdd044ffec51c692c61081e72ce34a56bec277
JinMo-source <qkrwslah123@naver.com>
2021-07-03 21:00:49 +0900

    tab부분 작업중

--- js/tab.js
+++ js/tab.js
@@ -1,15 +1,35 @@
 const tabNumber = document.querySelector('.bank-review__tab');
-
-const dataImg = {
-    t1 : {
-        img : "../img/tab-img1",
-        text : "b"
-    },
-}
+const tabBox = document.querySelector('.tab-image')
+const tabImg = document.querySelectorAll('.tab-image>div');
 
 function tabHandle(e){
     let dataNumber = e.target.dataset.img;
-    console.log(dataImg.b.img)
+    tabBox.innerHTML = "";
+    
+    for(let i =0; i < 8;i++){
+        let divImg = `<div class="tab-image__size"><img src="../img/tab-${dataNumber}"></div>`;
+        console.log(testImage('../bank/img/slider1.png'));
+        if(testImage('../bank/img/slider1.png')){
+            console.log('true');
+            return tabBox.insertAdjacentHTML('afterbegin', divImg);
+        }else if(!testImage('./img/slider1.png')){
+            console.log(false);
+            return alert('false');
+        }
+
+    }
+    
+}
+function testImage(URL) {
+    var tester=new Image();
+   tester.onload= isGood;
+   tester.onerror=isBad;
+   tester.src=URL;
+   console.log(tester.src);
+   function isGood() { console.log('true')
+       return true};
+   function isBad() { return false};
 }
 
+// testImage('./img/slider1.png')
 tabNumber.addEventListener('click',tabHandle);
